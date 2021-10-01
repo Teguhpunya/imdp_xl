96e118e141d922694f4032a094db9a7a16b5446c
Teguhpunya <teguhpunya.git@gmail.com>
2021-10-02 00:02:12 +0700

    Splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:imdp_xl/widgets/homescreen.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  // SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
   Firebase.initializeApp();
   runApp(MyApp());
 }
@@ -13,12 +15,56 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Quaildea',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const HomeScreen(),
+    return FutureBuilder(
+        future: Init.instance.initialize(),
+        builder: (context, AsyncSnapshot snapshot) {
+          // Show splash screen while waiting for app resources to load:
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return MaterialApp(home: Splash());
+          } else {
+            // Loading is done, return the app:,
+            return MaterialApp(
+              title: 'Quaildea',
+              theme: ThemeData(
+                primarySwatch: Colors.blue,
+              ),
+              home: const HomeScreen(),
+            );
+          }
+        });
+  }
+}
+
+class Init {
+  Init._();
+
+  static final instance = Init._();
+
+  Future initialize() async {
+    // This is where you can initialize the resources needed by your app while
+    // the splash screen is displayed.  Remove the following example because
+    // delaying the user experience is a bad design practice!
+    await Future.delayed(const Duration(seconds: 3));
+  }
+}
+
+class Splash extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    bool lightMode =
+        MediaQuery.of(context).platformBrightness == Brightness.light;
+    return Scaffold(
+      backgroundColor: lightMode
+          ? Color(0xffffff).withOpacity(1.0)
+          : Color(0x042a49).withOpacity(1.0),
+      body: Center(
+          child: lightMode
+              ? Image.asset(
+                  'assets/images/splash.jpg',
+                  colorBlendMode: BlendMode.screen,
+                )
+              // ? Icon(Icons.ac_unit)
+              : Image.asset('assets/splash_dark.png')),
     );
   }
 }
