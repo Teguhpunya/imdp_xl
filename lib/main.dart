4b458810764be8303136cbbf1f7035166ff9bc4f
Teguhpunya <teguhpunya.git@gmail.com>
2021-11-05 20:18:59 +0700

    Background service

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,62 @@
+import 'dart:async';
+import 'dart:convert';
+
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'package:flutter_background_service/flutter_background_service.dart';
 import 'package:imdp_xl/views/homescreen.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  Firebase.initializeApp();
+  await Firebase.initializeApp();
   runApp(MyApp());
+}
+
+onStart() {
+  WidgetsFlutterBinding.ensureInitialized();
+  final service = FlutterBackgroundService();
+  final dbRef = FirebaseDatabase.instance.reference();
+
+  service.onDataReceived.listen((event) {
+    if (event!["action"] == "setAsForeground") {
+      service.setForegroundMode(true);
+      return;
+    }
+
+    if (event["action"] == "setAsBackground") {
+      service.setForegroundMode(false);
+    }
+
+    if (event["action"] == "stopService") {
+      service.stopBackgroundService();
+    }
+  });
+
+  // bring to foreground
+  service.setForegroundMode(true);
+  Timer.periodic(Duration(seconds: 1), (timer) async {
+    if (!(await service.isServiceRunning())) timer.cancel();
+
+    // read data from firebase
+    var data, dataStr, dataJson;
+    double suhu;
+    dbRef.child('suhu').once().then((value) async {
+      data = await value.value[0];
+      dataStr = json.encode(data);
+      dataJson = json.decode(dataStr);
+      suhu = (dataJson['suhu1'] + dataJson['suhu2']) / 2;
+
+      service.setNotificationInfo(
+        title: "Quaildea",
+        content: "Suhu: $suhu° C",
+      );
+    });
+
+    // service.sendData(
+    //   {"current_date": DateTime.now().toIso8601String()},
+    // );
+  });
 }
 
 class MyApp extends StatelessWidget {
@@ -14,6 +64,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    FlutterBackgroundService.initialize(onStart);
+
     return FutureBuilder(
         future: Init.instance.initialize(),
         builder: (context, AsyncSnapshot snapshot) {
